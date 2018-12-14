265ac9371f2f8383e66f5c88496112ff473b4f19
Renj <itrenjunhua@163.com>
2018-12-14 13:24:42 +0800

    init commit project

---
+++ app/src/main/java/percentchart/renj/com/percentchartdemo/MainActivity.java
@@ -0,0 +1,13 @@
+package percentchart.renj.com.percentchartdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
